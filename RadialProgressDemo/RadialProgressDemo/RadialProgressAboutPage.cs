a0bdc9a25a01ed1ba34844cadd700c628a473444
jordfocus <jordfocus@gmail.com>
2016-01-16 16:29:14 +0100

    Added About page

---
+++ RadialProgressDemo/RadialProgressDemo/RadialProgressAboutPage.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace RadialProgressDemo
+{
+    public class RadialProgressAboutPage : ContentPage
+    {
+        public RadialProgressAboutPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            BackgroundImage = "AppBackground.jpg";
+            
+            Content = new StackLayout
+            {
+                Padding = 5,
+                VerticalOptions = LayoutOptions.Center,
+                Children =
+                {
+                    // Add text here
+                }
+            };
+
+        }
+    }
+}
